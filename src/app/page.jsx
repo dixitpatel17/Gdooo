039897ee5a0e9daa6b8941d8ade157251bf24a8f
dixitpatel <satapara678@gmail.com>
2024-04-29 17:44:16 +0530

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -31,7 +31,7 @@ function myProfile() {
                 <p className='text-[#D1D1D1] text-sm'> Check or redeem Swettballs</p>
               </div>
             </div>
-            <div className='self'>
+            <div>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 18L15 12L9 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
@@ -51,7 +51,7 @@ function myProfile() {
                 <p className='text-[#D1D1D1] text-sm'>Available Vouchers</p>
               </div>
             </div>
-            <div className='self'>
+            <div>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 18L15 12L9 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
@@ -72,8 +72,7 @@ function myProfile() {
                 <p className='text-[#D1D1D1] text-sm'>Your saved items</p>
               </div>
             </div>
-            <div className='self'>
-              
+            <div>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 18L15 12L9 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
